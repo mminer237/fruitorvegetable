1ea295783ca88622564862afa187767cbfd8c8ba
Matthew Miner <mminer237@gmail.com>
2019-11-07 08:37:25 -0500

    Implement search

--- js/search.js
+++ js/search.js
@@ -1,1 +1,8 @@
 "use strict"
+
+const searchBox = document.getElementById("search-box");
+document.querySelector("form").addEventListener("submit", e => {
+	const option = document.querySelector(`option[value=${searchBox.value}]`);
+	if (option)
+		window.location.href = option.dataset.url;
+});
