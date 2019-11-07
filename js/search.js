8c0e5db7f61058a14be30d9c4ed62a801dc1d10e
Matthew Miner <mminer237@gmail.com>
2019-11-07 08:45:18 -0500

    Prevent default

--- js/search.js
+++ js/search.js
@@ -2,6 +2,7 @@
 
 const searchBox = document.getElementById("search-box");
 document.querySelector("form").addEventListener("submit", e => {
+	e.preventDefault();
 	const option = document.querySelector(`option[value=${searchBox.value}]`);
 	if (option)
 		window.location.href = option.dataset.url;
