f993248c8c786ec08e6ac3964fb8563413abb88d
Matthew Miner <mminer237@gmail.com>
2019-11-07 20:52:50 -0500

    Improved/fixed search scripting

--- js/search.js
+++ js/search.js
@@ -1,11 +1,19 @@
 "use strict"
 
 const searchBox = document.getElementById("search-box");
+searchBox.addEventListener("input", e => {
+	if (getOption())
+		searchBox.setCustomValidity("");
+});
 document.querySelector("form").addEventListener("submit", e => {
 	e.preventDefault();
-	const option = document.querySelector(`option[value=${searchBox.value}]`);
+	const option = getOption();
 	if (option)
 		window.location.href = option.dataset.url;
 	else
 		searchBox.setCustomValidity("Invalid item.");
 });
+
+function getOption() {
+	return document.querySelector(`option[value="${searchBox.value}" i]`);
+}
